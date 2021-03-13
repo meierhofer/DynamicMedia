258fb9dd7dbc8469854fa2e60ad9ed6484e3110e
k.meierhofer <karoline.meierhofer@gmail.com>
2021-03-13 13:36:50 +0100

    made it work again

---
+++ Assets/Game/Scripts/Camera_ray.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_ray : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        var ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, 100) && hit.rigidbody != null)
+        {
+            hit.rigidbody.AddForce(Vector3.up, ForceMode.Impulse);
+        }
+    }
+}
